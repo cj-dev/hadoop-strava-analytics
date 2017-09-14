96fd82055570d0a470bf67ad8d2a5ddb20248a5c
Chris Julian <chris@zero1many.net>
2017-09-14 07:34:38 -0400

    Some mapper output formatting

--- mapper.py
+++ mapper.py
@@ -10,6 +10,8 @@ for line in sys.stdin:
     date_string = effort['start_date'].split(" ")[0]
     activity_id = effort['activity_id']
     elapsed_time = effort['elapsed_time']
+    if len(elapsed_time.split(":")[0]) < 2: # zero-pad the hour
+        elapsed_time = "0" + elapsed_time
 
-    print("{0}-{1}".format(date_string, activity_id), elapsed_time)
+    print("{0}#{1}".format(date_string, activity_id), elapsed_time)
 
